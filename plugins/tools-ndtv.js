d24e15579739c063aa29b4c0e14a3183c5131ed6
Tohid khan <154800445+tohidkhan6333@users.noreply.github.com>
2024-11-15 17:39:56 +0530

    Add files via upload

--- plugins/tools-ndtv.js
+++ plugins/tools-ndtv.js
@@ -20,7 +20,7 @@ let handler = async (m, { conn }) => {
 
     let newsInfo = `•───── ୨❀୧ ─────•
     ❖ 𝑺𝑻𝑨𝑻𝑼𝑺: Active
-    ㋡ 𝑪𝑹𝑬𝑨𝑻𝑶𝑹: Qasim
+    ㋡ 𝑪𝑹𝑬𝑨𝑻𝑶𝑹: Tohid
     ☞ 𝑯𝑬𝑨𝑫𝑳𝑰𝑵𝑬: ${newsItem.headline}
     ${newsItem.description}
     🔗 𝑹𝑬𝑨𝑫 𝑴𝑶𝑹𝑬: ${newsItem.url}
